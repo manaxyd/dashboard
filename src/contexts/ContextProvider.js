822ec42c6627131cbed53d85a3dc5c42bc5a5dcb
manaxyd <manaxyd@gmail.com>
2023-02-14 13:57:26 +0200

    header menu icons

--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -11,12 +11,15 @@ const initialState = {
 
 export const ContextProvider = ({ children }) => {
     const [activeMenu, setActiveMenu] = useState(true);
+    const [isClicked, setIsClicked] = useState(initialState);
 
     return (
         <StateContext.Provider
          value={{
             activeMenu: activeMenu,
             setActiveMenu,
+            isClicked,
+            setIsClicked
          }}
         >
          {children}
